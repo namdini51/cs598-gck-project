bb87e6c4da4defaa9f2916d1929f296a161a9964
namdini51 <donginn2@illinois.edu>
2025-02-08 16:03:30 -0600

    modified module and function to be more memory efficient.

--- simple_task/citation-network-analysis-nk.py
+++ simple_task/citation-network-analysis-nk.py
@@ -1,32 +1,25 @@
 """
-This python module conducts simple network tasks on citation network based on networkx package.
+This python module conducts simple network tasks on citation network based on networkit package.
 """
 
 import csv
 import random
-import networkx as nx
+import networkit as nk
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-
-def convert_data_to_graph(file, split_length=100000):
+def convert_data_to_graph_nk(file):
     """
-    This function converts the input data into a networkx graph and prints edge/node counts
-    :return: networkx graph object
+    This function converts the input data into a networkit graph and prints edge/node counts
+    :return: networkit graph object and node dictionary for reference
     """
 
-    G = nx.DiGraph()  # Directed graph (citing paper -> cited paper)
+    G = nk.graph.Graph(directed=True)
+    print("Checkpoint: Networkit Graph created!", flush=True)
 
-    # dtype_spec = {"citing": "str", "cited": "str"}
-    #
-    # # process by split data (handle OOM)
-    # split_count = 0
-    # for split in pd.read_csv(file, chunksize=split_length, dtype=dtype_spec):
-    #     edge_list = zip(split["citing"], split["cited"])  # Process only this chunk
-    #     G.add_edges_from(edge_list)
-    #     split_count += 1
+    node_dict = {}
+    next_node_idx = 0
 
     with open(file, "r") as f:
         dataset = csv.reader(f)
@@ -36,33 +29,47 @@ def convert_data_to_graph(file, split_length=100000):
 
         count = 0
         for row in dataset:
-            G.add_edge(row[citing_idx], row[cited_idx])
+            start = row[citing_idx]
+            end = row[cited_idx]
+
+            if start not in node_dict:
+                node_dict[start] = next_node_idx
+                G.addNode()
+                next_node_idx += 1
+
+            if end not in node_dict:
+                node_dict[end] = next_node_idx
+                G.addNode()
+                next_node_idx += 1
+
+            G.addEdge(node_dict[start], node_dict[end])
+
             count += 1
-            if count % 1000000 == 0:
+            if count % 10000000 == 0:
                 print(f"Checkpoint: Processed {count} edges...", flush=True)
 
-    edge_count = G.number_of_edges()
-    node_count = G.number_of_nodes()
+    edge_count = G.numberOfEdges()
+    node_count = G.numberOfNodes()
 
     print("Number of edges: ", edge_count)
     print("Number of nodes: ", node_count)
 
-    return G
+    return G, node_dict
 
 
 def plot_in_degree_distribution(G, output_path="./in_degree_distribution.png"):
     """
     This function plots the in-degree distribution of every node
-    :param G: networkx graph object
+    :param G: networkit graph object
     :param output_path: directory path to save output
     :return: None
     """
 
-    in_degree_list = []
-    for node, degree in G.in_degree():
-        in_degree_list.append(degree)
+    in_degree = nk.centrality.DegreeCentrality(G, outDeg=False)
+    in_degree.run()
+    in_degree_list = in_degree.scores()
 
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(15, 10))
     ax.hist(in_degree_list, bins=30, edgecolor='black', color="green", log=True) # log scale
 
     ax.set_xlabel("In-Degree (i.e., number of received citation)")
@@ -75,29 +82,30 @@ def plot_in_degree_distribution(G, output_path="./in_degree_distribution.png"):
 
 def extract_random_subgraph(G, num_subgraphs=5, min_node_count=100000):
     """
-    This function randomly extract connected components from a given graph based on Weakly Connected Components and converts them into subgraphs
-    :param G: networkx graph object
+    This function randomly extract connected components from a given graph and converts them into subgraphs
+    :param G: networkit graph object
     :param num_subgraphs: number of needed subgraphs
     :param min_node_count: minimum number of nodes in a subgraph
     :return: list of subgraphs
     """
 
-    weak_comp = nx.weakly_connected_components(G)
+    components = nk.components.WeaklyConnectedComponents(G).run().getComponents()
 
-    weak_comp_list = []
-    for comp in weak_comp:
+    comp_list = []
+    for comp in components:
         if len(comp) >= min_node_count:   # filter components that have more nodes than minimum node count
-            weak_comp_list.append(comp)
+            comp_list.append(comp)
 
     # randomly select
-    selected_comp = random.sample(weak_comp_list, min(num_subgraphs, len(weak_comp_list)))
+    selected_comp = random.sample(comp_list, min(num_subgraphs, len(comp_list)))
 
     # convert components to graphs
-    subgraphs = []
+    subgraph_list = []
     for comp in selected_comp:
-        subgraphs.append(G.subgraph(comp))
+        subgraph = nk.graphtools.subgraphFromNodes(G, comp)
+        subgraph_list.append(subgraph)
 
-    return subgraphs
+    return subgraph_list
 
 
 def calculate_node_degree_stats(subgraphs):
@@ -112,23 +120,23 @@ def calculate_node_degree_stats(subgraphs):
     node_degree_stats = []
 
     for g in subgraphs:
-        in_deg_list = []
-        for node, degree in g.in_degree():  # extract in-degree from all nodes
-            in_deg_list.append(degree)
+        in_degree = nk.centrality.DegreeCentrality(g, outDeg=False)
+        in_degree.run()
+        in_degree_list = in_degree.scores()
 
-        total_deg_list = []
-        for node, degree in g.degree():  # extract total degree from all nodes
-            total_deg_list.append(degree)
+        total_degree = nk.centrality.DegreeCentrality(g, outDeg=True)
+        total_degree.run()
+        total_degree_list = total_degree.scores()
 
         # in-degree calculation
-        in_deg_min = min(in_deg_list)
-        in_deg_max = max(in_deg_list)
-        in_deg_median = float(np.median(in_deg_list))
+        in_deg_min = min(in_degree_list)
+        in_deg_max = max(in_degree_list)
+        in_deg_median = float(np.median(in_degree_list))
 
         # total degree calculation
-        total_deg_min = min(total_deg_list)
-        total_deg_max = max(total_deg_list)
-        total_deg_median = float(np.median(total_deg_list))
+        total_deg_min = min(total_degree_list)
+        total_deg_max = max(total_degree_list)
+        total_deg_median = float(np.median(total_degree_list))
 
         subgraph_node_deg_stats = {
             "Minimum In-Degree": in_deg_min,
@@ -146,7 +154,7 @@ def calculate_node_degree_stats(subgraphs):
 
 if __name__ == '__main__':
     file_path = "./data/sample_open_citations_curated.csv"
-    citation_graph = convert_data_to_graph(file_path)
+    citation_graph, node_dict = convert_data_to_graph_nk(file_path)
     # print(citation_graph)
 
     plot_in_degree_distribution(citation_graph)
